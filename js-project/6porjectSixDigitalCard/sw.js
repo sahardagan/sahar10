4a3468231b886ce3f8a7390fb8b217fbd70c353f
sahar dagan <sahardagannn@gmail.com>
2024-09-05 16:44:28 +0300

    sasfd

--- js-project/6porjectSixDigitalCard/sw.js
+++ js-project/6porjectSixDigitalCard/sw.js
@@ -1,54 +1,28 @@
-// sw.js
-
-const CACHE_NAME = 'v1'; // שם של המטמון
+const CACHE_NAME = 'my-cache-v1';
 const urlsToCache = [
   '/',
   '/index.html',
   '/style.css',
-  '/icon/192x192.png',
-  '/icon/512x512.png',
-  '/front.jpg',
-  '/back.jpg',
-  // הוסף כאן את כל הנתיבים שאתה רוצה למטמון
+  '/icon/android-icon-192x192.png',
+  // הוסף משאבים נוספים כאן
 ];
 
-// Event to install service worker and cache necessary files
 self.addEventListener('install', (event) => {
-  console.log('Service Worker installing.');
-
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then((cache) => {
-        console.log('Caching app shell');
         return cache.addAll(urlsToCache);
+      })
+      .catch((error) => {
+        console.error('Failed to cache resources:', error);
       })
   );
 });
 
-// Event to activate service worker
-self.addEventListener('activate', (event) => {
-  console.log('Service Worker activating.');
-
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (cacheName !== CACHE_NAME) {
-            console.log('Clearing old cache:', cacheName);
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
-});
-
-// Fetch event to serve cached files
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request)
       .then((response) => {
-        // Return cached response if found, else fetch from network
         return response || fetch(event.request);
       })
   );
