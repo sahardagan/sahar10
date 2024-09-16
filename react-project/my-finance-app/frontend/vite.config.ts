68e538d32c502d3acb7eeb11ff935ad0c61884d8
sahar dagan <sahardagannn@gmail.com>
2024-09-16 16:19:05 +0300

    sss

--- react-project/my-finance-app/frontend/vite.config.ts
+++ react-project/my-finance-app/frontend/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/my-project/", // כאן עדכן לנתיב שבו רץ האתר שלך
   plugins: [react()],
   server: {
     port: 5173, // ודא שהפורט נכון
